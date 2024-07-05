f83bf1c5e0c7a5dd661fb5e1c6ee7ef99f2819de
Kanchan <kanchandhamala1@gmail.com>
2024-07-05 13:55:54 +0545

    origin main

---
+++ 4_containers.js
@@ -0,0 +1,19 @@
+const id=13
+let email="asdf@google.com"
+var password=12455
+city="Pokkhara"
+let accountstate;
+
+// id=3
+email="aaa@gmail.com"
+password=3333
+city="Kathmandu"
+
+console.log(id);
+console.table([id,email,password,city,accountstate]);
+
+/* const can be used to variable constant
+ variable can be make variable by using var or let
+Use let as variable variable  */
+
+
