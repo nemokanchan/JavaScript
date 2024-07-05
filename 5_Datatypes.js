6a18535b5732b094dafacee032fb5ab7d7fb4636
Kanchan <kanchandhamala1@gmail.com>
2024-07-05 18:43:39 +0545

    initial commit

---
+++ 5_Datatypes.js
@@ -0,0 +1,25 @@
+ "use strict"  // It treats all as Js Code as newer version
+
+//  alert(3+4)  we are using node js not browser
+// code readibility should be high
+console.log(3+3); 
+console.log("Kanchan");
+
+let name="Kanchan"   //string
+let age=16          //number
+let isloggedin=true //boolean
+
+//number=>2 tothe power 53
+//binget
+/*string >=""
+boolean=>true or false
+null :Stand alone Value
+undefined :has no value 
+symbol : to find uniqueness*/
+
+//object 
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
