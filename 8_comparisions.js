40405ea1ad307cb7f1b46c67559330216623d149
Kanchan <kanchandhamala1@gmail.com>
2024-07-07 23:11:17 +0545

    initial commit

---
+++ 8_comparisions.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("2">1);//output=true
+console.log("02">1);//output=true
+
+console.log(null >0);//output =false
+console.log(null ==0);//output=false
+console.log(null >=0);//output=true
+
+console.log(undefined == 0);//false
+console.log(undefined > 0);//false
+console.log(undefined < 0);//false
+
+//  ====
+
+console.log("2" === 2); //No conversion so false
+
+//Note:Clean code is the most important thing
