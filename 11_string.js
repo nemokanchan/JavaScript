3b4bc5305dd5dec8d0296b7b36ad9be07c286720
Kanchan <kanchandhamala1@gmail.com>
2024-07-09 23:02:05 +0545

    initial commit

---
+++ 11_string.js
@@ -0,0 +1,32 @@
+const name="Kanchan"
+const repocount=7
+
+console.log(name + repocount + "Value");
+
+console.log(`Hello! my name is ${name} and I have ${repocount} repositories`);
+
+const gamename= new String("COC");
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+
+console.log(gamename.length);
+
+console.log(gamename.toLowerCase());
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf(2));
+
+
+const newstring =gamename.substring(0,2)
+
+console.log(newstring);
+
+const anotherstring = gamename.slice(-1,2)
+console.log(anotherstring);
+
+// trim function is used to remove the space of start and end 
+
+// replace is used to can be used to replace some string by other string
+
+// include function is used to search or to  ask question
+
