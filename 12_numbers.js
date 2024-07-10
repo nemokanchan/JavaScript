0d76d486893188aa3f631c1a5b22087b199dadb6
Kanchan <kanchandhamala1@gmail.com>
2024-07-10 22:34:48 +0545

    initial commit

---
+++ 12_numbers.js
@@ -0,0 +1,17 @@
+const score=300
+console.log(score);
+
+const balance=new Number(90);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const othernum =23.3423555
+
+console.log(othernum.toPrecision(3));
+
+const hundereds=1000000
+console.log(hundereds.toLocaleString('en-in'));
+
