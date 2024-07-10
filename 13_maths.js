61d97c8d332dc5b092196e82869852597393a50c
Kanchan <kanchandhamala1@gmail.com>
2024-07-10 22:47:18 +0545

    initial commit

---
+++ 13_maths.js
@@ -0,0 +1,16 @@
+console.log(Math);//In browser consol you can get various functions of math
+
+console.log(Math.abs(-4));
+console.log(Math.round(2.4));
+console.log(Math.ceil(2.4));
+console.log(Math.floor(2.4));
+console.log(Math.min(2,4,1,6,7,9));
+console.log(Math.max(2,4,1,6,7,9));
+console.log(Math.random());
+
+const min =10
+const max =20
+
+console.log(Math.random()*(max-min +1)+min)
+
+// Random gives values between 0 and 1 and when we multiply it by 10 the no. shifts to left
