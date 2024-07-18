483cc98a7e61592f92dee37507a5ce2cd95ea04f
Kanchan <kanchandhamala1@gmail.com>
2024-07-18 11:32:42 +0545

    initial commit

---
+++ 19_objectDestruction.js
@@ -0,0 +1,23 @@
+const fruit={
+    name:"Mango",
+    price:80,
+    market:"Balkhu"
+}
+
+// fruit.name
+
+const {price:rs}=fruit
+console.log(rs);
+
+// Jason:
+
+{
+    name : "Apple"
+
+}
+
+[
+    {},
+    {},
+    {}
+]
