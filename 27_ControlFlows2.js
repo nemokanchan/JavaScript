5eb6f90d1313cc8979265d161f8d56e493b5a585
Kanchan <kanchandhamala1@gmail.com>
2024-09-15 22:27:27 +0545

    initial commit

---
+++ 27_ControlFlows2.js
@@ -0,0 +1,39 @@
+// switch(key) {
+//     case value:
+
+//     break;
+
+//     default:
+//         break;
+// }
+
+const days= 3
+
+
+
+switch (days) {
+  case 1:
+    console.log("Sunday")
+    break;
+
+    
+    case 2:
+        console.log("Monday")
+    break;
+    
+    case 3:
+        console.log("Tuesday")
+    break;
+        
+    case 4:
+        console.log("Wednesday")
+    break;
+    case 5:
+        console.log("Thursday")
+    break;
+    
+    default:
+        console.log("Default match");
+            
+    break;
+}
