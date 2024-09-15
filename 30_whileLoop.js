88130de9298724e7c42f8d92ab4fbe75bbfe1a15
Kanchan <kanchandhamala1@gmail.com>
2024-09-15 23:39:11 +0545

    initial commit

---
+++ 30_whileLoop.js
@@ -0,0 +1,25 @@
+// While:::::::::::
+let i=0;
+while (i<=5) {
+    console.log(`Value of index is ${i}`);
+    i= i+1;
+    
+
+}
+
+
+let arr = ['flash' , 'batman' , 'superman']
+let a=0;
+while(a< arr.length){
+    console.log(`Value is ${arr[a]}`);
+    a= a+1;
+    
+}
+
+let score =1
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+    
+} while (score<=10);
